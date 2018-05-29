7c2fb58e33c271079465328967f563727d7cdbe1
martin <karlito40@gmail.com>
2018-05-29 13:54:01 +0200

    scope placeholder to policies handler

--- src/policies.ts
+++ src/policies.ts
@@ -2,6 +2,7 @@ import ShowConversationFriendGate from "./auth/gate/ShowConversationFriendGate";
 import GateManager from "./common/gate/GateManager";
 import { Request } from "express";
 import { User } from "./entity/user";
+import { replace } from "./util/string";
 
 export const policies = [{
   scope: "show-conversation-friend", gate: ShowConversationFriendGate
@@ -11,10 +12,13 @@ export function getPolicy(scope: string) {
   return policies.find(policy => policy.scope == scope);
 }
 
-export async function isScopeAuthorized(scopes: Array<string>, req: Request, user: User) {
+export async function isScopesAuthorized(scopes: Array<string>, req: Request, user: User) {
   if (!scopes.length) {
     return true;
   }
+
+  const placeholders = {...req.query, ...{ me: user.id }};
+  scopes = scopes.map(scope => replace(scope, placeholders));
 
   return (
     await createGateManager(scopes, req, user).isAuthorized()
@@ -26,7 +30,7 @@ export function createGateManager(scopes: Array<string>, req: Request, user: Use
   const manager = new GateManager();
   scopes.forEach(scope => {
     const policy = getPolicy(scope);
-    if (policy) {
+    if (policy && policy.gate) {
       manager.add(new policy.gate(req, user));
     }
   });
